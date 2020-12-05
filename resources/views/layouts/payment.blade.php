fd330fe3822218b5e4ab777e9e669203064edd00
hponemahn <hponemahn@gmail.com>
2020-12-05 16:14:53 +0630

    increase file size

--- resources/views/layouts/payment.blade.php
+++ resources/views/layouts/payment.blade.php
@@ -144,7 +144,7 @@
 
         $(document).on('change', '.file', function () {
 
-            if(this.files[0].size > 307200){
+            if(this.files[0].size > 407200){
                 alert("File is too big!");
                 this.value = "";
             };
