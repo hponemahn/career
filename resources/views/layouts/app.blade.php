a177668cdf59f23999bf9f47ec4b743d6f981fff
hponemahn <hponemahn@gmail.com>
2021-01-10 10:44:16 +0630

    add facebook pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,120 +1,168 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
+
 <head>
 
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-K0T5GZF2C9"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K0T5GZF2C9"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-	gtag('config', 'G-K0T5GZF2C9');
-	</script>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-	<!-- Primary Meta Tags -->
-	<title>မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး</title>
-	<meta name="title" content="မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး">
-	<meta name="description" content="မိမိရဲ့ ပင်ကိုစရိုက်၊ ပင်ကို အရည်အသွေးအပေါ်မူတည်ပြီး မိမိနဲ့ သင့်တော်ကိုက်ညီမယ့် အလုပ်အကိုင် (၅) မျိုးကို အကြံပြု ဖော်ပြပေးသွားမှာ ဖြစ်ပါတယ်">
+        gtag('config', 'G-K0T5GZF2C9');
 
-	<meta name="keywords" content="mymar, mymar digital, mymar tech, career advice, အလုပ်အကိုင်, ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသောအကောင်းဆုံးအလုပ်(၅)မျိုး, ပင်ကိုစရိုက်၊">
-	<meta name="robots" content="index, follow">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="author" content="Phone Mhan">
+    </script>
 
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://mymar.digital/career">
-	<meta property="og:title" content="မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး">
-	<meta property="og:description" content="မိမိရဲ့ ပင်ကိုစရိုက်၊ ပင်ကို အရည်အသွေးအပေါ်မူတည်ပြီး မိမိနဲ့ သင့်တော်ကိုက်ညီမယ့် အလုပ်အကိုင် (၅) မျိုးကို အကြံပြု ဖော်ပြပေးသွားမှာ ဖြစ်ပါတယ်">
-	<meta property="og:image" content="https://mymar.digital/images/mymar_career_meta_preview.png">
+    <!-- Facebook Pixel Code -->
+    <script>
+        ! function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '859579081470632');
+        fbq('track', 'PageView');
 
-	<!-- Twitter -->
-	<meta property="twitter:card" content="summary_large_image">
-	<meta property="twitter:url" content="https://mymar.digital/career">
-	<meta property="twitter:title" content="မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး">
-	<meta property="twitter:description" content="မိမိရဲ့ ပင်ကိုစရိုက်၊ ပင်ကို အရည်အသွေးအပေါ်မူတည်ပြီး မိမိနဲ့ သင့်တော်ကိုက်ညီမယ့် အလုပ်အကိုင် (၅) မျိုးကို အကြံပြု ဖော်ပြပေးသွားမှာ ဖြစ်ပါတယ်">
-	<meta property="twitter:image" content="https://mymar.digital/images/mymar_career_meta_preview.png">
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=859579081470632&ev=PageView&noscript=1" /></noscript>
+    <!-- End Facebook Pixel Code -->
 
-	<!-- Stylesheets
+    <!-- Primary Meta Tags -->
+    <title>မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး</title>
+    <meta name="title" content="မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး">
+    <meta name="description"
+        content="မိမိရဲ့ ပင်ကိုစရိုက်၊ ပင်ကို အရည်အသွေးအပေါ်မူတည်ပြီး မိမိနဲ့ သင့်တော်ကိုက်ညီမယ့် အလုပ်အကိုင် (၅) မျိုးကို အကြံပြု ဖော်ပြပေးသွားမှာ ဖြစ်ပါတယ်">
+
+    <meta name="keywords"
+        content="mymar, mymar digital, mymar tech, career advice, အလုပ်အကိုင်, ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသောအကောင်းဆုံးအလုပ်(၅)မျိုး, ပင်ကိုစရိုက်၊">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="author" content="Phone Mhan">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://mymar.digital/career">
+    <meta property="og:title" content="မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး">
+    <meta property="og:description"
+        content="မိမိရဲ့ ပင်ကိုစရိုက်၊ ပင်ကို အရည်အသွေးအပေါ်မူတည်ပြီး မိမိနဲ့ သင့်တော်ကိုက်ညီမယ့် အလုပ်အကိုင် (၅) မျိုးကို အကြံပြု ဖော်ပြပေးသွားမှာ ဖြစ်ပါတယ်">
+    <meta property="og:image" content="https://mymar.digital/images/mymar_career_meta_preview.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://mymar.digital/career">
+    <meta property="twitter:title" content="မိမိရဲ့ ကိုယ်ရည်ကိုယ်သွေးနှင့်ကိုက်ညီသော အကောင်းဆုံး အလုပ် ၅ မျိုး">
+    <meta property="twitter:description"
+        content="မိမိရဲ့ ပင်ကိုစရိုက်၊ ပင်ကို အရည်အသွေးအပေါ်မူတည်ပြီး မိမိနဲ့ သင့်တော်ကိုက်ညီမယ့် အလုပ်အကိုင် (၅) မျိုးကို အကြံပြု ဖော်ပြပေးသွားမှာ ဖြစ်ပါတယ်">
+    <meta property="twitter:image" content="https://mymar.digital/images/mymar_career_meta_preview.png">
+
+    <!-- Stylesheets
 	============================================= -->
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('asset/css/bootstrap.css') }}" type="text/css" />
-	{{-- <link rel="stylesheet" href="style.css" type="text/css" /> --}}
-	<link rel="stylesheet" href="{{ asset('asset/css/style.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('asset/css/dark.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('asset/css/font-icons.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('asset/css/animate.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('asset/css/magnific-popup.css') }}" type="text/css" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i"
+        rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/bootstrap.css') }}" type="text/css" />
+    {{-- <link rel="stylesheet" href="style.css" type="text/css" /> --}}
+    <link rel="stylesheet" href="{{ asset('asset/css/style.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/dark.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/font-icons.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/animate.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/magnific-popup.css') }}" type="text/css" />
 
-	<!-- DatePicker CSS -->
-	{{-- <link rel="stylesheet" href="{{ asset('css/datepicker.css') }}" type="text/css" /> --}}
+    <!-- DatePicker CSS -->
+    {{-- <link rel="stylesheet" href="{{ asset('css/datepicker.css') }}" type="text/css" /> --}}
 
-	<link rel="stylesheet" href="{{ asset('asset/css/responsive.css') }}" type="text/css" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="{{ asset('asset/css/responsive.css') }}" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-	<link rel="stylesheet" href="{{ asset('asset/css/photographer.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('asset/css/custom.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/photographer.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('asset/css/custom.css') }}" type="text/css" />
 
-	
-	<link rel="shortcut icon" href="{{ asset('images/favicon-32x32.png') }}" />
 
-	{{-- myanmar web font --}}
-	<link rel="stylesheet" href='https://mmwebfonts.comquas.com/fonts/?font=myanmar3' />
-	<link rel="stylesheet" href='https://mmwebfonts.comquas.com/fonts/?font=zawgyi' />
+    <link rel="shortcut icon" href="{{ asset('images/favicon-32x32.png') }}" />
 
-	<style>
-		.form-group > label.error {
-			display: block !important;
-			text-transform: none;
-		}
+    {{-- myanmar web font --}}
+    <link rel="stylesheet" href='https://mmwebfonts.comquas.com/fonts/?font=myanmar3' />
+    <link rel="stylesheet" href='https://mmwebfonts.comquas.com/fonts/?font=zawgyi' />
 
-		.form-group input.valid ~ label.error,
-		.form-group input[type="text"] ~ label.error,
-		.form-group input[type="email"] ~ label.error,
-		.form-group input[type="number"] ~ label.error,
-		.form-group select ~ label.error { display: none !important; }
+    <style>
+        .form-group>label.error {
+            display: block !important;
+            text-transform: none;
+        }
 
-		label .pricing-box { background-color: #F8F9FA; }
-		label:not(.checked) .pricing-box { background-color: #FFF; .custom-padding-bottom }
+        .form-group input.valid~label.error,
+        .form-group input[type="text"]~label.error,
+        .form-group input[type="email"]~label.error,
+        .form-group input[type="number"]~label.error,
+        .form-group select~label.error {
+            display: none !important;
+        }
 
-        .custom-padding-bottom > label {
+        label .pricing-box {
+            background-color: #F8F9FA;
+        }
+
+        label:not(.checked) .pricing-box {
+            background-color: #FFF;
+            .custom-padding-bottom
+        }
+
+        .custom-padding-bottom>label {
             padding-bottom: 15px;
         }
 
-		/* myanmar web font */
-		.zawgyi{
-			font-family:Zawgyi-One;
-		}
+        /* myanmar web font */
+        .zawgyi {
+            font-family: Zawgyi-One;
+        }
 
-		.unicode{
-			font-family:Myanmar3,Yunghkio,'Masterpiece Uni Sans';
-		}
+        .unicode {
+            font-family: Myanmar3, Yunghkio, 'Masterpiece Uni Sans';
+        }
 
-		* {
-			font-family:Myanmar3,Yunghkio,'Masterpiece Uni Sans';
-		}
+        * {
+            font-family: Myanmar3, Yunghkio, 'Masterpiece Uni Sans';
+        }
 
-	</style>
+    </style>
 
 </head>
 
 <body class="stretched">
 
-	<!-- Document Wrapper
+    <!-- Document Wrapper
 	============================================= -->
-	<div id="wrapper" class="clearfix">
+    <div id="wrapper" class="clearfix">
 
-		<!-- Header
+        <!-- Header
 		============================================= -->
-		<header id="header" class="transparent-header no-sticky">
+        <header id="header" class="transparent-header no-sticky">
 
-			<div id="header-wrap">
-				<div class="container">
-					<a href="{{ url('/') }}"><img src="{{ asset('images/myMarLogo.png') }}" alt="MyMar Logo" height="100"></a>
-				</div>
-		
-				{{-- <div>
+            <div id="header-wrap">
+                <div class="container">
+                    <a href="{{ url('/') }}"><img src="{{ asset('images/myMarLogo.png') }}" alt="MyMar Logo"
+                            height="100"></a>
+                </div>
+
+                {{-- <div>
 					<div class="primary-menu-trigger hamburger">
 						<i class="icon-line-menu"></i>
 						<i class="icon-line-cross"></i>
@@ -128,55 +176,58 @@
 						</div>
 					</div>
 				</div> --}}
-			</div>
-		
-		</header>
-		<!-- #header end -->
+            </div>
 
-		<!-- Content
+        </header>
+        <!-- #header end -->
+
+        <!-- Content
         ============================================= -->
         @yield('content')
         <!-- #content end -->
 
-		<!-- Footer
+        <!-- Footer
 		============================================= -->
-		<footer class="py-4 flex-shrink-0" style="background: #F5F5F5; padding-bottom: 0 !important;">
-			<div class="container text-center">
-			  {{-- <a href="https://bootstrapious.com/snippets" class="text-muted">Bootstrap snippet by Bootstrapious</a> --}}
-			  © MyMar. 2020. All Rights Reserved.
-			  <br>
-			  <br>
-				<a href="https://www.facebook.com/mymartech" target="_blank" class="social-icon inline-block si-small si-borderless si-rounded si-facebook">
-					<i class="icon-facebook"></i>
-					<i class="icon-facebook"></i>
-				</a>
-				<a href="tel:09407709504" target="_blank" class="social-icon inline-block si-small si-borderless si-rounded si-twitter">
-					<i class="icon-phone"></i>
-					<i class="icon-phone"></i>
-				</a>
-			</div>
-		  </footer>
-		<!-- #footer end -->
+        <footer class="py-4 flex-shrink-0" style="background: #F5F5F5; padding-bottom: 0 !important;">
+            <div class="container text-center">
+                {{-- <a href="https://bootstrapious.com/snippets" class="text-muted">Bootstrap snippet by Bootstrapious</a> --}}
+                © MyMar. 2020. All Rights Reserved.
+                <br>
+                <br>
+                <a href="https://www.facebook.com/mymartech" target="_blank"
+                    class="social-icon inline-block si-small si-borderless si-rounded si-facebook">
+                    <i class="icon-facebook"></i>
+                    <i class="icon-facebook"></i>
+                </a>
+                <a href="tel:09407709504" target="_blank"
+                    class="social-icon inline-block si-small si-borderless si-rounded si-twitter">
+                    <i class="icon-phone"></i>
+                    <i class="icon-phone"></i>
+                </a>
+            </div>
+        </footer>
+        <!-- #footer end -->
 
-	</div><!-- #wrapper end -->
+    </div><!-- #wrapper end -->
 
-	<!-- Go To Top
+    <!-- Go To Top
 	============================================= -->
-	<div id="gotoTop" class="icon-angle-up"></div>
+    <div id="gotoTop" class="icon-angle-up"></div>
 
-	<!-- External JavaScripts
+    <!-- External JavaScripts
 	============================================= -->
-	<script src="{{ asset('asset/js/jquery.js') }}"></script>
-	<script src="{{ asset('asset/js/plugins.js') }}"></script>
+    <script src="{{ asset('asset/js/jquery.js') }}"></script>
+    <script src="{{ asset('asset/js/plugins.js') }}"></script>
 
-	<!-- DatePicker JS -->
-	{{-- <script src="{{ asset('asset/js/datepicker.js') }}"></script> --}}
+    <!-- DatePicker JS -->
+    {{-- <script src="{{ asset('asset/js/datepicker.js') }}"></script> --}}
 
-	<!-- Footer Scripts
+    <!-- Footer Scripts
 	============================================= -->
-	<script src="{{ asset('asset/js/functions.js') }}"></script>
+    <script src="{{ asset('asset/js/functions.js') }}"></script>
 
-	@stack('javascript')
+    @stack('javascript')
 
 </body>
+
 </html>
