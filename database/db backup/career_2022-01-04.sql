0cbd28a42896aafc9fb33b6fda531fb9999e3883
hponemahn <hponemahn@gmail.com>
2022-01-04 18:55:23 +0630

    db backup

---
+++ database/db backup/career_2022-01-04.sql
@@ -0,0 +1,139 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.30)
+# Database: career
+# Generation Time: 2022-01-04 12:24:58 PM +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table migrations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`)
+VALUES
+	(1,'2014_10_12_000000_create_users_table',1),
+	(2,'2014_10_12_100000_create_password_resets_table',1),
+	(3,'2016_01_01_000000_add_voyager_user_fields',1),
+	(4,'2016_01_01_000000_create_data_types_table',1),
+	(5,'2016_05_19_173453_create_menu_table',1),
+	(6,'2016_10_21_190000_create_roles_table',1),
+	(7,'2016_10_21_190000_create_settings_table',1),
+	(8,'2016_11_30_135954_create_permission_table',1),
+	(9,'2016_11_30_141208_create_permission_role_table',1),
+	(10,'2016_12_26_201236_data_types__add__server_side',1),
+	(11,'2017_01_13_000000_add_route_to_menu_items_table',1),
+	(12,'2017_01_14_005015_create_translations_table',1),
+	(13,'2017_01_15_000000_make_table_name_nullable_in_permissions_table',1),
+	(14,'2017_03_06_000000_add_controller_to_data_types_table',1),
+	(15,'2017_04_21_000000_add_order_to_data_rows_table',1),
+	(16,'2017_07_05_210000_add_policyname_to_data_types_table',1),
+	(17,'2017_08_05_000000_add_group_to_settings_table',1),
+	(18,'2017_11_26_013050_add_user_role_relationship',1),
+	(19,'2017_11_26_015000_create_user_roles_table',1),
+	(20,'2018_03_11_000000_add_user_settings',1),
+	(21,'2018_03_14_000000_add_details_to_data_types_table',1),
+	(22,'2018_03_16_000000_make_settings_value_nullable',1),
+	(23,'2019_08_19_000000_create_failed_jobs_table',1),
+	(24,'2020_11_26_091345_users',1),
+	(25,'2020_11_26_094246_payments',1),
+	(26,'2020_11_26_094309_types',1),
+	(27,'2020_11_26_094320_results',1);
+
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table results
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `results`;
+
+CREATE TABLE `results` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `selected_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payment` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `receipt` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `secret` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `is_done` tinyint(4) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `results` WRITE;
+/*!40000 ALTER TABLE `results` DISABLE KEYS */;
+
+INSERT INTO `results` (`id`, `user_id`, `selected_type`, `payment`, `receipt`, `secret`, `is_done`, `created_at`, `updated_at`)
+VALUES
+	(1,1,'estj','w','','',0,NULL,NULL),
+	(2,2,'estj','c','','',0,NULL,NULL);
+
+/*!40000 ALTER TABLE `results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) unsigned DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT 'users/default.png',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_role_id_foreign` (`role_id`),
+  CONSTRAINT `users_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `role_id`, `name`, `phone`, `email`, `avatar`, `created_at`, `updated_at`)
+VALUES
+	(1,NULL,'w','3',NULL,'users/default.png','2020-12-08 02:56:26','2020-12-08 02:56:26'),
+	(2,NULL,'c','4',NULL,'users/default.png','2020-12-08 02:57:37','2020-12-08 02:57:37');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
